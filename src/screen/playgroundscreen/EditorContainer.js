73a561b1377fd23a2296f14b3ccb736494e6b04c
Pranay Solanki <160771787+PranaySolanki@users.noreply.github.com>
2025-09-10 13:12:57 +0530

    playground screen buttons changed to icons....by pranay

--- src/screen/playgroundscreen/EditorContainer.js
+++ src/screen/playgroundscreen/EditorContainer.js
@@ -3,6 +3,8 @@ import { useRef, useState } from "react";
 import "./EditorContainer.scss"
 import Editor from "@monaco-editor/react";
 import  executeCode  from "./CompilerAPI";
+import { PlayArrow,DarkModeOutlined,LightModeOutlined,FileDownloadOutlined,FileUploadOutlined,FullscreenOutlined } from "@mui/icons-material";
+import { Tooltip } from "@mui/material";
 
 const EditorContainer = ({ onCodeRun, input, theme, setTheme }) => {
 
@@ -115,6 +117,25 @@ const EditorContainer = ({ onCodeRun, input, theme, setTheme }) => {
                 </div>
 
                 <div className="editor-right-container">
+                    <Tooltip title="Full Screen" placement="bottom" arrow enterDelay={100}>
+                        <button className="btn" onClick={fullscreen} disabled={isProcessing} >
+                            <FullscreenOutlined className="material-symbols-outlined"/>
+                        </button>
+                    </Tooltip>
+                <Tooltip title="Upload Code" placement="bottom" arrow enterDelay={100}>  
+                    <label htmlFor="import-code" className="btn" disabled={isProcessing} >
+                    <FileUploadOutlined className="material-symbols-outlined"/>
+                    </label>
+                    <input type="file" id="import-code" style={{display: "none"}} onChange={ImportCode} disabled={isProcessing}/>
+                </Tooltip>
+
+                <Tooltip title="Download Code" placement="bottom" arrow enterDelay={100}>
+                    <button className="btn" onClick={exportCode} disabled={isProcessing} >
+                        <FileDownloadOutlined className="material-symbols-outlined" />
+                    </button>
+                </Tooltip>
+
+
                     <select onChange={onChangeLanguage} value={language} disabled={isProcessing}>
                         <option value="c">C</option>
                         <option value="java">Java</option>
@@ -124,9 +145,13 @@ const EditorContainer = ({ onCodeRun, input, theme, setTheme }) => {
 
                     <button onClick={onChangeTheme} className="theme-toggle-btn" disabled={isProcessing}>
                         {theme === 'vs-light' ? (
-                            <span className="material-symbols-outlined">dark_mode</span>
+                            <Tooltip title="Dark Mode" placement="bottom" arrow enterDelay={100}>
+                                <DarkModeOutlined className="material-symbols-outlined dark"/>
+                            </Tooltip>
                         ) : (
-                            <span className="material-symbols-outlined">light_mode</span>
+                            <Tooltip title="Light Mode" placement="bottom" arrow enterDelay={100}>
+                                <LightModeOutlined className="material-symbols-outlined light"/>
+                            </Tooltip>
                         )}
                      </button>
 
@@ -153,7 +178,7 @@ const EditorContainer = ({ onCodeRun, input, theme, setTheme }) => {
                 />
             </div>
             <div className={`editor-footer ${theme === 'vs-light' ? 'light-theme' : 'dark-theme'}`}>
-                <button className="btn" onClick={fullscreen} disabled={isProcessing} >
+                {/* <button className="btn" onClick={fullscreen} disabled={isProcessing} >
                     <span className="material-icons">fullscreen</span>
                     <span>Full Screen</span>
                 </button>
@@ -165,9 +190,9 @@ const EditorContainer = ({ onCodeRun, input, theme, setTheme }) => {
                 <button className="btn" onClick={exportCode} disabled={isProcessing} >
                     <span className="material-icons" >cloud_upload</span>
                     <span>Export Code</span>
-                </button>
-                <button className="btn" onClick={RunCode} disabled={isProcessing} >
-                    <span className="material-icons">play_arrow</span>
+                </button> */}
+                <button className="Runbtn" onClick={RunCode} disabled={isProcessing} >
+                    <PlayArrow className="material-icons"/>
                     <span>{isProcessing ? "Running..." : "Run Code"}</span>
                 </button>
             </div>
